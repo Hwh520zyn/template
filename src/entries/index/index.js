e8f57fb847f3e95ba67b18a712d7a02376266f1f
weihai han <weihaihan@WHMac.local>
2018-08-17 19:12:28 +0800

    add: 首页pc端开发

--- src/entries/index/index.js
+++ src/entries/index/index.js
@@ -1,1 +1,101 @@
 import '@/css/index.less'
+import 'swiper/dist/css/swiper.min.css'
+import Swiper from 'swiper/dist/js/swiper.min.js'
+import * as Validate from '@/utils/validateRegExp'
+import $ from 'jquery'
+new Swiper('.swiper-container', {
+  effect: 'fade',
+  autoplay: {
+    disableOnInteraction: false
+  }
+//   pagination: {
+//     el: '.swiper-pagination'
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev'
+//   }
+})
+
+class ValidateForm {
+  constructor ({ el, validateReg, msgCallback = () => { } }) {
+    Object.assign(this, {
+      el: $(el) || [],
+      validateReg: validateReg || [],
+      result: null,
+      sum: 0,
+      msgCallback
+    })
+    this.init()
+  }
+  /**
+   * 初始化
+  */
+  init () {
+    this.validateEvent()
+  }
+  /**
+   * 正则验证事件
+   */
+  validateEvent () {
+    this.el.map((index, item) => {
+      item.blur(() => {
+        const regExp = this.validateReg[index]
+        if (item.val() === '') {
+          item.parent().addClass('empty-error').removeClass('error').removeClass('success')
+        } else if (!regExp.test(item.val())) {
+          item.parent().addClass('error').removeClass('success').removeClass('empty-error')
+        } else {
+          item.parent().addClass('success').removeClass('error').removeClass('empty-error')
+        }
+      })
+    })
+  }
+  /**
+   * 按钮提交事件
+   */
+  btnEvent () {
+    this.sum = 0
+    this.el.map((index, item) => {
+      if (item.val() === '') {
+        item.parent().addClass('empty-error').removeClass('success').removeClass('error')
+      }
+      if (item.parent().hasClass('error') || item.parent().hasClass('empty-error')) {
+        console.log('hasClass error')
+        this.sum++
+      } else {
+        // console.log(item.hasClass('error'))
+        const info = item.parent().parent().serializeArray()
+        this.result = info
+      }
+    })
+    if (this.sum === 0) {
+      this.msgCallback(this.result)
+    }
+  }
+}
+/**
+ *  new
+ */
+const valiate = new ValidateForm({
+  el: [
+    $('#name-form'),
+    $('#hosp-form'),
+    $('#num-form')
+  ],
+  validateReg: [
+    Validate.REALNAME,
+    Validate.COMPANY,
+    Validate.CELLPHONE
+  ],
+  btn: $('#btn-form'),
+  msgCallback (res) {
+    console.log(res)
+  }
+})
+
+$('#btn-form').on('click', function (e) {
+  e.preventDefault()
+  valiate.btnEvent()
+})
+
--- src/layout/default.js
+++ src/layout/default.js
@@ -3,8 +3,7 @@ import '@/css/layout.less'
 import 'babel-polyfill'
 import '@/font/iconfont.css'
 // import isMobile from '@/utils/isMobile'
-import 'swiper/dist/css/swiper.min.css'
-import Swiper from 'swiper/dist/js/swiper.min.js'
+
 import $ from 'jquery'
 // $('.head-box').css({'z-index':2})
 // const hei = $('.head-box').outerHeight()
@@ -12,7 +11,12 @@ import $ from 'jquery'
 
 $('.head-box-wrapper-nav').css({'zIndex': 2})
 $('.back-top').css({'zIndex': 10})
-$('.back-top-back').on('click', function () {
+$('.banner-btn').css({'zIndex': 10})
+$('.foot-box-wrapper').css({'zIndex': 10})
+const Hei = $('.foot-box').outerHeight()
+$('.foot-place').outerHeight(Hei)
+
+$('#back-top').on('click', function () {
   $('html,body').animate({'scrollTop': 0}, 500)
 })
 
@@ -21,13 +25,3 @@ $('.mobile-toggle-nav').on('click', function () {
   $('.head-box-wrapper-nav').toggleClass('show')
 })
 
-new Swiper('.swiper-container', {
-  effect: 'fade',
-  autoplay: {
-    disableOnInteraction: false
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-})
