7cc662cacf23da73934c532e10e1660d61caa7ae
weihai han <weihaihan@WHMac.local>
2018-08-24 16:45:29 +0800

    add: 详情页增加分享

--- src/entries/interdetail/index.js
+++ src/entries/interdetail/index.js
@@ -43,38 +43,7 @@ new HbdVideo({
     // hide_h5_setting: true
   }
 })
-//分享划过切换
-$('.main-sharebox-share-hover-icon li').on('mouseenter', function () {
-  const index = $(this).index()
-  $('.main-sharebox-share-hover-cont li').eq(index).addClass('show-li').siblings().removeClass('show-li')
-})
 
-$('.main-sharebox-share').on('click', function () {
-  $('.main-sharebox-share-hover').toggleClass('show-share')
-})
-
-$('.mobile-share-brower').css({'zIndex': 999})
-
-// 移动端分享
-
-$('.other-nav-box-share').on('click', function () {
-  $('.mobile-share-brower').show()
-  $('body').addClass('over-body').removeClass('body-auto')
-})
-
-$('.mobile-share-brower ').on('click', '.share-cancle', function () {
-  $('.share-box').show()
-  $('.link-box').hide()
-  $(this).parent().hide()
-  $('body').addClass('body-auto').removeClass('over-body')
-})
-
-$('#icon-box-wx-friends, #icon-box-wx-circle').on('click', function () {
-  const url = location.href
-  $('.share-box').hide()
-  $('.link-box').show()
-  $('.link-box-textarea').html(url)
-})
 
 $('.prevent').on('click', function (e) {
   e.preventDefault()
