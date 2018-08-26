f918e8b62dcc6448b1aada4a8dd367ae7cdb311b
weihai han <weihaihan@WHMac.local>
2018-08-26 14:50:34 +0800

    add: 添加分享上传参数

--- src/entries/info/index.js
+++ src/entries/info/index.js
@@ -2,7 +2,7 @@ import '@/css/info.less'
 import Tabs from '@/components/tabs'
 import '@/css/articlelist.less'
 import PageList from '@/components/page-list'
-// import PullList from '@/components/pull-list'
+import PullList from '@/components/pull-list'
 // import Loading from '@/components/loading'
 // import isMobile from '@/utils/isMobile'
 import Api from '@/utils/api'
@@ -38,7 +38,7 @@ function page (el, pageel, type) {
                             <div class="hbd-card1-content-detail-experts">
                                 ${data.articleDate.slice(0, 10)}
                             </div>
-                            <div class="hbd-card1-content-detail-experts-cont textRight main-sharebox-like"><i class="iconfont icon-z-like"></i> <span href="/likearticle/${data.id}">点赞</span> </div>
+                            <div class="hbd-card1-content-detail-experts-cont textRight main-sharebox-like"><i class="iconfont icon-z-like"></i> <span> 点赞 </span></div>
                         </div>
                     </div>
                 </div>
@@ -46,7 +46,7 @@ function page (el, pageel, type) {
       </div>`
       }
     },
-    //  ${data.articleDate.slice(0, 10)}
+    //  ${data.articleDate.slice(0, 10)} /likearticle/${data.id}
     api: Api.infoPageList,
     onSuccess (res) {
       let items = res.results.items.map(item => {
@@ -135,46 +135,6 @@ $(async function () {
     }
   }
 })
-// $('.hbd-tabs-header').on('click', 'li', async function () {
-//   const index = $(this).index()
-//   if (index === 0) {
-//     const param = {
-//       id: 43
-//     }// 最新资讯
-//     const res = await Api.infoPageList(param)
-//     window.page_list[index].pageChangeToDo(1, res)
-//   } else if (index === 1) {
-//     const param = {
-//       id: 39631
-//     }// 行业人物
-//     const res = await Api.infoPageList(param)
-//     window.page_list[index].pageChangeToDo(1, res)
-//   } else if (index === 2) {
-//     const param = {
-//       id: 8928
-//     }// 医疗热点
-//     const res = await Api.infoPageList(param)
-//     window.page_list[index].pageChangeToDo(1, res)
-//   } else if (index === 3) {
-//     const param = {
-//       id: 7972
-//     }// 医患关系
-//     const res = await Api.infoPageList(param)
-//     window.page_list[index].pageChangeToDo(1, res)
-//   } else if (index === 4) {
-//     const param = {
-//       id: 444
-//     }// 医院管理
-//     const res = await Api.infoPageList(param)
-//     window.page_list[index].pageChangeToDo(1, res)
-//   } else if (index === 5) {
-//     const param = {
-//       id: 3380
-//     }// 学术人文
-//     const res = await Api.infoPageList(param)
-//     window.page_list[index].pageChangeToDo(1, res)
-//   }
-// })
 // 分页1
 page('#bottom-list', '#bottomPagination', 'news')
 // 分页2
@@ -199,12 +159,14 @@ $('#focus input').on('blur', function () {
 $('.hbd-list_content').on('click', function (e) {
   var ev = e || event
   var target = ev.target || ev.srcElement
-  // const id = target.parentNode.parentNode.parentNode.previousElementSibling.querySelector('.set').getAttribute('href')
+  const id = target.parentNode.parentNode.parentNode.previousElementSibling.querySelector('.set').getAttribute('href')
   // console.log(id)
-  // const newid = id.replace(/[^0-9]/ig, '')
+  const newid = id.replace(/[^0-9]/ig, '')
+  Api.likearticle({id: newid}).then((res) => {})
+  console.log(newid)
   if (target.nodeName.toLowerCase() === 'span') {
     target.innerHTML = '已点赞'
-    target.parentElement.className += ' ' + 'active-like'
+    target.parentElement.parentElement.className += ' ' + 'active-like'
   }
 })
 
