651464b88519797074745ec7e8fcfe0d9c5d6b04
weihai han <weihaihan@WHMac.local>
2018-08-30 14:08:51 +0800

    add: 修复导航刷新问题

---
+++ src/entries/casedetail/index.js
@@ -0,0 +1,51 @@
+import '@/css/infodetail.less'
+import '@/css/info.less'
+import '@/css/publicdetail.less'
+import '@/css/casedetail.less'
+import '@/css/interdetail.less'
+import Tabs from '@/components/tabs'
+import $ from 'jquery'
+import DxyShare from '@dxy/pure-components/dist/dxyShare'
+import Api from '@/utils/api'
+new Tabs({
+  el: '.main-interdebox-interde-contbox-cont-left',
+  defaultActive: 1
+})
+$('.prevent').on('click', function (e) {
+  e.preventDefault()
+})
+let num = 14
+$('.plus').on('click', function () {
+  let numplus = ++num
+  $('.main-bottom-list-item-txtbox p').css({'fontSize': numplus + 'px'})
+})
+$('.less').on('click', function () {
+  let numplus = --num
+  $('.main-bottom-list-item-txtbox p').css({'fontSize': numplus + 'px'})
+})
+// 分享
+new DxyShare({
+  container: '#dxy-share-sina',
+  channels: ['sina']
+})
+new DxyShare({
+  container: '#dxy-share-qzone',
+  channels: ['qzone']
+})
+
+// 移动端分享
+new DxyShare({
+  container: '#icon-box-qzone',
+  channels: ['sina']
+})
+new DxyShare({
+  container: '#icon-box-wb',
+  channels: ['qzone']
+})
+
+$('.like-inter, .icon-z-like').on('click', function (e) {
+  const id = location.href
+  const newid = id.replace(/[^0-9]/ig, '')
+  // console.log(newid)
+  Api.likearticle({id: newid}).then((res) => {})
+})
